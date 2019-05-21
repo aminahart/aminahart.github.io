8129748691e991fba3d6659be3b42a2278293146
aminahart <50748842+aminahart@users.noreply.github.com>
2019-05-21 10:02:27 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,38 +1,35 @@
 var ball;
 var balls = [];
-var x;
 var numballs;
 var clicks = 0;
 var removing = false
 
 function setup() {
- //createCanvas(1425, 600);
-  createCanvas(displayWidth, displayHeight)
+  createCanvas(displaywidth, displayheight);
+  // createCanvas(800, 600)
   colE = createElement('h1', 'Choose a ball color');
-  colE.position(0, displayheight - 100);
+  colE.position(0, 400);
   col = createInput();
-  col.position(0, displayheight - 80);
+  col.position(0, 400 + 115);
 
   backE = createElement('h1', 'Choose a background color');
-  backE.position(0, displayheight - 60);
+  backE.position(0, 400 + 120);
   back = createInput('grey');
-  back.position(0, displayheight - 40);
+  back.position(0, 400 + 180);
   x = createElement('h1', 'There are ' + clicks + ' balls')
-  x.position(400, displayheight - 100)
+  x.position(400, 400 + 60)
 
   reset = createButton('reset')
-  reset.position(displaywidth - 40, displayheight - 80)
+  reset.position(width - 40, 400 + 80)
   reset.mousePressed(update)
   
   i = createElement('h1', 'Click the screen')
-  i.position( 420, height - 120)
+  i.position( 420, 400 + 120)
 }
 
 function mousePressed() {
   if (mouseX <= width && mouseY <= height && removing == false) {
     var b = new Ball(random(0, 255), random(0, 255), random(0, 255), col.value());
-    // print('radi' ,b.r)
-    // print('color' ,b.col)
     balls.push(b);
     clicks += 1;
   }
@@ -78,9 +75,47 @@ function draw() {
 
   if (keyIsDown(32)) {
     removeballs()
-    // print('remove')
-    // removing = true
-    // }else{
-    //   removing = false
+  }
+}
+function removeballs() {
+  // if (mousePressed && removing == true){
+  for (var i = balls.length - 1; i >= 0; i--) {
+    if (balls[i].clicked(mouseX, mouseY)) {
+      balls.splice(i, 1);
+      clicks -= 1
+      // }
+    }
+  }
+}
+
+function update() {
+  for (var i = balls.length - 1; i >= 0; i--) {
+    balls.splice(i, 1);
+  }
+  clicks = 0
+}
+
+function draw() {
+  background(back.value());
+  for (var b of balls) {
+    b.show();
+    b.move();
+    b.check();
+    var overlapping = false
+    for (var other of balls) {
+      if (b !== other && b.intersects(other)) {
+        overlapping = true
+      }
+    }
+    if (overlapping) {
+      b.changecolor(225)
+    } else {
+      b.changecolor(90)
+    }
+  }
+  x.html('There are ' + clicks + ' balls')
+
+  if (keyIsDown(32)) {
+    removeballs()
   }
 }
